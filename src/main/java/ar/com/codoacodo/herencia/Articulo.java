3db626822d7669c68a4c904a3ebc293adb9b7e06
ecastro20b <elsacastrob@gmail.com>
2022-12-28 12:29:57 -0500

    cambios x

---
+++ src/main/java/ar/com/codoacodo/herencia/Articulo.java
@@ -0,0 +1,20 @@
+package ar.com.codoacodo.herencia;
+
+public class Articulo {
+	String img;
+	String titulo;
+	String autor;
+	float precio;
+	
+	// alt+shift+S oara generar constructor
+	public Articulo(String img, String titulo, String autor, float precio) {
+		super();
+		this.img = img;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.precio = precio;
+	}
+	
+	
+
+}
---
+++ src/main/java/ar/com/codoacodo/herencia/Buscador.java
@@ -0,0 +1,15 @@
+package ar.com.codoacodo.herencia;
+
+public class Buscador {
+	String claveBusqueda;
+	Articulo[] resultados;
+	
+	Buscador(){
+		
+	}
+	
+	void buscar(){
+		
+	}
+
+}
